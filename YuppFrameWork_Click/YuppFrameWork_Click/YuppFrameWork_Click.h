87d6d64671a4470a4c8474176f2a40cbecd3517f
khaleel123 <khaleel.lulla@gmail.com>
2025-02-10 14:44:39 +0530

    Add files via upload

---
+++ YuppFrameWork_Click/YuppFrameWork_Click/YuppFrameWork_Click.h
@@ -0,0 +1,18 @@
+//
+//  YuppFrameWork_Click.h
+//  YuppFrameWork_Click
+//
+//  Created by Khaleel on 10/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YuppFrameWork_Click.
+FOUNDATION_EXPORT double YuppFrameWork_ClickVersionNumber;
+
+//! Project version string for YuppFrameWork_Click.
+FOUNDATION_EXPORT const unsigned char YuppFrameWork_ClickVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YuppFrameWork_Click/PublicHeader.h>
+
+
